ecf9c464d80f9fbcedb05b9c7b97a4a88cd57ed9
Narayan Bandodker <repomaster@phaidelta.com>
2023-08-10 11:22:31 +0530

    Writing output to a folder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is needed for Windows PC using scaling (150% scale) only
 import ctypes
+
 ctypes.windll.shcore.SetProcessDpiAwareness(True)
 
 
@@ -7,9 +8,10 @@ import cv2
 import argparse
 import numpy as np
 from pathlib import Path
+from typing import Optional
 
 
-def make_image_sharp(image, blur_amt = 0):
+def make_image_sharp(image, blur_amt=0):
     # tune to get better result
 
     # fmt: off
@@ -22,11 +24,7 @@ def make_image_sharp(image, blur_amt = 0):
     image = cv2.filter2D(image, -1, kernel)
     # image = cv2.GaussianBlur(image, (1, 1), blur_amt)
 
-    kernel = np.array([
-        [1, 2, 1],
-        [2, 8, 2],
-        [1, 2, 1]
-    ]) / 16.0  # Normalize the kernel
+    kernel = np.array([[1, 2, 1], [2, 8, 2], [1, 2, 1]]) / 16.0  # Normalize the kernel
 
     # Apply the custom kernel using filter2D
     filtered_image = cv2.filter2D(image, -1, kernel)
@@ -35,11 +33,7 @@ def make_image_sharp(image, blur_amt = 0):
 
 
 def enhance(img):
-    kernel = np.array([
-        [1, 1, 1],
-        [2, 4, 2],
-        [1, 1, 1]
-    ]) / 16.0  # Normalize the kernel
+    kernel = np.array([[1, 1, 1], [2, 4, 2], [1, 1, 1]]) / 16.0  # Normalize the kernel
 
     # # Apply the custom kernel using filter2D
     # filtered_image = cv2.filter2D(img, -1, kernel)
@@ -74,34 +68,64 @@ def preprocess(img):
     return gray
 
 
-def handle_img_file(img_file: Path):
+def handle_img_file(img_file: Path, save_path: Optional[Path] = None):
     img = cv2.imread(str(img_file))
-    cv2.imshow("Image", resize_image(img))
+    if img is None:
+        print("Skipping %s as it is not a valid image." % str(img_file))
+        return
+
+    should_display = True
+    if save_path is not None:
+        should_display = False
+
+    if should_display:
+        cv2.imshow("Image", resize_image(img))
 
     preprocessed = preprocess(img)
-    if preprocessed is not None:
+
+    if should_display and preprocessed is not None:
         cv2.imshow("Preprocessed", preprocessed)
 
-    return cv2.waitKey(0)
+    if save_path is not None:
+        img_save_path = save_path.joinpath(img_file.name)
+        save_path.mkdir(parents=True, exist_ok=True)
+        cv2.imwrite(str(img_save_path), preprocessed)
+
+    if should_display:
+        return cv2.waitKey(0)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-i", "--image", action="store", type=Path, required=True, help="Image"
+        "-i",
+        "--image",
+        action="store",
+        type=Path,
+        required=True,
+        help="Image file/directory",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=Path,
+        required=False,
+        help="If used, output directory to save preprocessed images (disables window output)",
     )
     args = parser.parse_args()
 
     # Either filename or directory name passed as argument
     img_path: Path = args.image
+    save_path: Optional[Path] = args.output
 
     if img_path.is_dir():
-        for i, img_file in enumerate(img_path.glob("*.jpg")):
-            key = handle_img_file(img_file)
+        for img_file in img_path.glob("*"):
+            key = handle_img_file(img_file, save_path)
             # Quit if q key is pressed
-            if key == ord('q'): break
+            if key == ord("q"):
+                break
     elif img_path.exists():
-        handle_img_file(img_path)
+        handle_img_file(img_path, save_path)
 
 
 if __name__ == "__main__":
